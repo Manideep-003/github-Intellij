dbc469f6f093ff452b67904829e1c4b9df948f04
ManideepBotsa <manideepbotsa@gmail.com>
2025-02-13 20:06:59 +0530

    init commit

--- BinarySearch/src/comm/man/SmallestLetterGreaterThantheTarget.java
+++ BinarySearch/src/comm/man/SmallestLetterGreaterThantheTarget.java
@@ -1,36 +1,40 @@
 package comm.man;
 
-public class BSearch {
+public class SmallestLetterGreaterThantheTarget {
     public static void main(String[] args) {
-        int [] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 20};
-        int ans = binarysearch(arr, 20);
+        int [] arr={2,3,9,14,16,18};
+        int target=15;
+        int ans=smallest(arr,target);
         System.out.println(ans);
     }
-    static int binarysearch(int [] arr,int target)
+    static int smallest(int [] letters,int target)
     {
         int start = 0;
-        int end = arr.length - 1; // Adjusted to arr.length - 1 to include the last index
-        boolean isAsc=arr[start]<arr[end];
+        int end = letters.length - 1; // Adjusted to arr.length - 1 to include the last index
+        boolean isAsc=letters[start]<letters[end];
         while (start<= end) { // Changed from l < r to l <= r to include the last element
             int mid = start + (end-start) / 2; // Moved inside the loop
-            if (target == arr[mid]) { // Corrected comparison
-                return arr[mid];
-            }
+
             if(isAsc){
-                if (target < arr[mid]) {
+                if (target < letters[mid]) {
                     end = mid - 1;
                 } else {
                     start= mid + 1;
                 }
             }
             else{
-                if (target > arr[mid]) {
+                if (target > letters[mid]) {
                     end = mid - 1;
                 } else {
                     start= mid + 1;
                 }
             }
         }
-        return -1;
+
+       if (start== letters.length){
+
+           return letters[0];
+       }
+       return letters[start];
     }
 }
